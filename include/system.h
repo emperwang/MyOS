7f3c92ab22b4f689e6ed4522d0bbe5d4aaec589a
wangkai <544094478@qq.com>
2017-09-04 10:55:07 +0800

    Add the display driver(screen.c)

--- include/system.h
+++ include/system.h
@@ -10,4 +10,14 @@ extern unsigned char inportb(unsigned short _port);
 extern void outportb(unsigned short _port,unsigned char _data);
 
 
+/*fucntion in screen.c*/
+extern void scroll(void);
+extern void move_csr(void);
+extern void cls(void);
+extern void putch(unsigned char c);
+extern void puts(unsigned char* str);
+extern void settextcolor(unsigned char forecolor,backcolor);
+extern void init_video(void);
+
+
 #endif

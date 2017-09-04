546891ca6b4e052f9eb5242f0ec829589adb099d
wangkai <544094478@qq.com>
2017-09-04 09:34:34 +0800

    completion the function in main.c

--- main.c
+++ main.c
@@ -1,35 +1,50 @@
 #include <system.h>
+
+
 unsigned char *memcpy(unsigned char *dest,const unsigned char *src,int count)
 {
-
+   const char *sp = (const char*)src;
+   char * dp = (char *) dest;
+   for(;count!=0;count--)
+       *dp = *sp++;
+   return dest;
 }
 
 unsigned char *memset(unsigned char *dest,unsigned char cal,int count)
 {
-
+    char *temp = (char *)dest;
+    for(;count != 0;count--)
+        *temp++ = val;
+    return dest;
 }
 
 unsigned short *memsetw(unsigned short *dest,unsigned short val,int count
 )
 {
-    
+    unsigned short *temp = (unsigned short*)dest;
+    for(;count!=0;count--)
+        *temp++=val;
+    return dest;
 }
 
 
 int strlen(const char *str)
 {
-    
+     int retval;
+     for(retval=0;*str != '\0';str++)
+         retval++;
+     return retval;
 }
 
 /*We will usr this to write to I/O ports to send bytes to devices.This 
 will be used in the next tutorial for changing the textmode cursor
 position.Again,we use some inline assemble for the stuff that simply
 cannot be done in c*/
-nsigned char inportb(unsigned short _port)
+unsigned char inportb(unsigned short _port)
 {
     unsigned char rv;
     __asm__ __volatile__("inb %1,%0"
-            `           :"=a"(rv)
+                        :"=a"(rv)
                         :"dN"(_port));
     return rv;
 }
